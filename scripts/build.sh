3ac26bdf104fae23b198160b8fb9f11ff6b35695
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-10-26 22:54:10 +0900

    update output

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,25 +1,25 @@
 set -e
 
-rm -rf .bin
+rm -rf bin
 cd src
 
 echo 'building darwin-amd64...'
-GOOS=darwin GOARCH=amd64 go build -o ../.bin/darwin-amd64/faroe
+GOOS=darwin GOARCH=amd64 go build -o ../bin/darwin-amd64/faroe
 echo 'building darwin-arm64...'
-GOOS=darwin GOARCH=arm64 go build -o ../.bin/darwin-arm64/faroe
+GOOS=darwin GOARCH=arm64 go build -o ../bin/darwin-arm64/faroe
 
 echo 'building linux-amd64...'
-GOOS=linux GOARCH=amd64 go build -o ../.bin/linux-amd64/faroe
+GOOS=linux GOARCH=amd64 go build -o ../bin/linux-amd64/faroe
 echo 'building linux-arm64...'
-GOOS=linux GOARCH=arm64 go build -o ../.bin/linux-arm64/faroe
+GOOS=linux GOARCH=arm64 go build -o ../bin/linux-arm64/faroe
 
 echo 'building windows-amd64...'
-GOOS=windows GOARCH=amd64 go build -o ../.bin/windows-amd64/faroe
+GOOS=windows GOARCH=amd64 go build -o ../bin/windows-amd64/faroe
 echo 'building windows-arm64...'
-GOOS=windows GOARCH=arm64 go build -o ../.bin/windows-arm64/faroe
+GOOS=windows GOARCH=arm64 go build -o ../bin/windows-arm64/faroe
 
 cd ..
-cd .bin
+cd bin
 for dir in $(ls -d *); do
     cp ../LICENSE "$dir"/LICENSE
     zip -r "$dir".zip $dir
